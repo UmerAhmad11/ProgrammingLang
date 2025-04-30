f0de6852ac5c1ea270d2a136f256b036d74a1292
UmerAhmad11 <sumerahmad2002@gmail.com>
2025-04-30 23:18:56 +0300

    Lexer (done)

--- src/main.c
+++ src/main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include "include/lexer.h"
 
 int main(int argc, char* argv[]){
-    printf("Hello UmerAhmad11");
-    return 0;
+    lexer_T* lexer = init_lexer(
+        "var name = \"John Doe\";\n"
+        "print(name);\n"
+    );
+
+
+    token_T* token = NULL; 
+
+    
+    while ((token = lexer_get_next_token(lexer)) != NULL) {
+        
+        if (token && token->value) {
+             printf("TOKEN(%d, %s)\n", token->type, token->value);
+        } else if (token) {
+             
+             printf("TOKEN(%d, (null value))\n", token->type);
+        }
+        
+    }
 }
