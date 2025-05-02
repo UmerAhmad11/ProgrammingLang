037572664ee5df534f7d645bf05a51b3fd4e897b
UmerAhmad11 <sumerahmad2002@gmail.com>
2025-05-02 16:42:50 +0300

    Parser - 1

---
+++ src/AST.c
@@ -0,0 +1,31 @@
+#include "include/AST.h"
+#include <stdlib.h>
+
+AST_T* init_ast(int type)
+{
+    AST_T* ast = calloc(1, sizeof(struct AST_STRUCT));
+    ast->type = type;
+    return ast;
+
+    /* AST VARIABLE DEFINITION */
+    ast->variable_definition_name = (void*)0;
+    ast->variable_definition_value = (void*)0;
+
+    /* AST VARIABLE */
+    ast->variable_name=(void*)0;
+
+
+    /* AST FUNCTION CALL */
+    ast->function_call_name=(void*)0;
+    ast->function_call_arguments;
+    ast->function_call_arguments_size=0;
+
+    /* AST STRING */
+    ast->string_value=(void*)0;
+
+    /* AST COMPOUND*/
+    ast->compound_value=(void*)0;
+    ast->compound_size=0;
+
+
+}
